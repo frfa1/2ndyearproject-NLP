fc5ed83725524ac07520bb79ae3f17380789dc92
Frederik Høngaard <mail@frederikhoengaard.com>
2021-03-19 16:22:06 +0100

    Update unigram.py

--- unigram.py
+++ unigram.py
@@ -92,16 +92,20 @@ def  main():
     clf = UnigramModel()
     clf.fit(reviews, targets)
 
-    use_test = False
+    use_test = True
     if use_test:
-        unseen = pd.read_json('data/music_reviews_test.json', lines=True)[['reviewText','sentiment']].dropna()
+        unseen = pd.read_json('data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']].fillna(value=' ')
     else:
-        unseen = pd.read_json('data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']].dropna()
+        unseen = pd.read_json('data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']].fillna(value=' ')
     new_reviews, new_targets = unseen['reviewText'], unseen['sentiment']
 
     y_pred = clf.predict(new_reviews)
 
-    print(accuracy(new_targets,y_pred))
+    
+    #print(accuracy(new_targets,y_pred))
+
+    df = pd.DataFrame(data=y_pred,columns=['sentiment'])
+    df.to_csv('predictions.csv')
 
 if __name__ == '__main__':
     main()
