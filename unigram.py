6f80322bc98ed94da3ee5d18d4036f135f0ad501
Frederik Høngaard <mail@frederikhoengaard.com>
2021-03-19 15:29:52 +0100

    Update unigram.py

--- unigram.py
+++ unigram.py
@@ -97,7 +97,7 @@ def  main():
     clf = UnigramModel()
     clf.fit(reviews, targets)
 
-    dev_data = pd.read_json('data/music_reviews_test.json', lines=True)[['reviewText','sentiment']]#.dropna()
+    dev_data = pd.read_json('data/music_reviews_test.json', lines=True)[['reviewText','sentiment']].dropna()
     dev_reviews, dev_targets = dev_data['reviewText'], dev_data['sentiment']
 
     y_pred = clf.predict(dev_reviews)
