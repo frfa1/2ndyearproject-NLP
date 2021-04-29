76b688258a23b3e74e705b647fb81a2ce34ce0ce
Frederik Høngaard <mail@frederikhoengaard.com>
2021-04-29 13:04:14 +0200

    Create loader.py

---
+++ utils/loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def load_train(drop=True):
+    data = pd.read_json('../data/music_reviews_train.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')
+
+
+
+
+def main():
+    dat = load_train(drop=True)
+    print(dat.info)
+
+
+if __name__ == '__main__':
+    main()
