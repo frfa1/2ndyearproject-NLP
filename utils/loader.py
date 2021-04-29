d93cf9dd60a73eb627b02c3b9ce3b742f3708f2c
Frederik Høngaard <mail@frederikhoengaard.com>
2021-04-29 13:07:39 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -7,12 +7,16 @@ def load_train(drop=True):
     else:
         return data.fillna('')
 
-
+def load_dev(drop=True):
+    data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')
 
 
 def main():
     dat = load_train(drop=True)
-    print(dat.info)
 
 
 if __name__ == '__main__':
