a19a12c7c8a4864536bc0458eec9efa8d4be5d77
Frederik Høngaard <mail@frederikhoengaard.com>
2021-04-29 13:17:28 +0200

    Create loader.py

---
+++ data/loader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def load_train(drop=True) -> pd.DataFrame:
+    data = pd.read_json('../data/music_reviews_train.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')
+
+def load_dev(drop=True) -> pd.DataFrame:
+    data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')
+
+def load_test(drop=True) -> pd.DataFrame:
+    data = pd.read_json('../data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')     
+
+
+def main():
+    train = load_train()
+    dev = load_dev()
+    test = load_test()
+
+if __name__ == '__main__':
+    main()
