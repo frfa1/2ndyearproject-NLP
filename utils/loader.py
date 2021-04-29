5dfb32a98929f01c925e80ecb662db5b1b761145
Frederik Høngaard <mail@frederikhoengaard.com>
2021-04-29 13:12:08 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,23 +1,32 @@
 import pandas as pd
 
-def load_train(drop=True):
+
+def load_train(drop=True) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_train.json', lines=True)[['reviewText','sentiment']]
     if drop:
         return data.dropna()
     else:
         return data.fillna('')
 
-def load_dev(drop=True):
+def load_dev(drop=True) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
     if drop:
         return data.dropna()
     else:
         return data.fillna('')
 
+def load_test(drop=True) -> pd.DataFrame:
+    data = pd.read_json('../data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']]
+    if drop:
+        return data.dropna()
+    else:
+        return data.fillna('')     
+
 
 def main():
-    dat = load_train(drop=True)
-
+    train = load_train()
+    dev = load_dev()
+    test = load_test()
 
 if __name__ == '__main__':
     main()
