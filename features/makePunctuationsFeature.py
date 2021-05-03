bc2601cfc1197e3d685331c6ec4041b62bfa144f
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-04 00:04:01 +0200

    Update makePunctuationsFeature.py

--- features/makePunctuationsFeature.py
+++ features/makePunctuationsFeature.py
@@ -2,33 +2,26 @@ import loader
 import re
 from sys import argv
 
-def get_exclamation_marks(data, regex_pattern="[\w']+|[!?]", export=False):
-    
-    re_object = re.compile(regex_pattern)
-    
-    exclamation_marks = [len([word for word in re.findall(regex_pattern, review) if word == '!']) for review in data]
 
+def get_exclamation_marks(data, regex_pattern="!", export=False):
+    re_object = re.compile(regex_pattern)
+    exclamation_marks = [len(re.findall(regex_pattern, review)) for review in data]
     if export:
         with open('../data/exclamation_mark_count.txt','w') as pc:
-            for line in exclamation_marks:
-                pc.write(str(line)+'\n')
-    
-    
-    return exclamation_marks
+            for item in exclamation_marks:
+                pc.write(str(item)+'\n')
+    else:
+        return exclamation_marks
 
-def get_question_marks(data, regex_pattern="[\w']+|[!?]", export=False):
-    
+def get_question_marks(data, regex_pattern="\?", export=False):
     re_object = re.compile(regex_pattern)
-    
-    question_marks = [len([word for word in re.findall(regex_pattern, review) if word == '?']) for review in data]
-
+    question_marks = [len(re.findall(regex_pattern, review)) for review in data]
     if export:
         with open('../data/question_mark_count.txt','w') as qc:
-            for line in question_marks:
-                qc.write(str(line)+'\n')
-    
-    
-    return question_marks
+            for item in question_marks:
+                qc.write(str(item)+'\n')
+    else:
+        return question_marks
 
 
 def main():
@@ -36,8 +29,11 @@ def main():
     data = loader.load_train()['reviewText'].tolist()
 
     if 'export' in args:
-        exclamation_count_out = get_exclamation_marks(data, export=True)
-        question_count_out = get_question_marks(data, export=True)
+        get_exclamation_marks(data, export=True)
+        get_question_marks(data, export=True)
+
+   # print(get_exclamation_marks(data))
+   # print(get_exclamation_marks(data))
 
 if __name__ == '__main__':
     main()
