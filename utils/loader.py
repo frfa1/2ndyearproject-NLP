913fe926eb7654ba7e2a44ec249cfadceac55cda
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-19 11:56:17 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
 
-def load_train(drop=True) -> pd.DataFrame:
+def load_train(drop=True,balance=True) -> pd.DataFrame:
     data = pd.read_json('https://github.itu.dk/raw/frph/2ndyearproject/master/data/music_reviews_train.json?token=AAAAWLY264NANGVT3WFCFDLAVYG2A', lines=True)[['reviewText','sentiment']]
+
+    if balance:
+        pos = data.loc[data["sentiment"] == "positive"]
+        neg = data.loc[data["sentiment"] == "negative"]
+        if len(pos) > len(neg):
+            bigger = pos
+            smaller = neg
+        elif len(neg) > len(pos):
+            bigger = neg
+            smaller = pos
+
+        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
+        data = pd.concat([bigger, smaller]).reset_index(drop=True)
+
     if drop:
-       return data.dropna().reset_index(drop=True)
+        return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')
 
@@ -27,6 +41,7 @@ def main():
     train = load_train()
     dev = load_dev()
     test = load_test()
+
 
 if __name__ == '__main__':
     main()
