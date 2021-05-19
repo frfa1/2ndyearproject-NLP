78fd00eae54d70ccbaa64d92c21d5324bcc61157
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-19 11:01:30 +0200

    Update loader.py

--- features/loader.py
+++ features/loader.py
@@ -2,39 +2,31 @@ import pandas as pd
 
 
 def load_train(drop=True) -> pd.DataFrame:
-    data = pd.read_json('../data/music_reviews_train.json', lines=True)[['reviewText','sentiment']]
+    data = pd.read_json('https://github.itu.dk/raw/frph/2ndyearproject/master/data/music_reviews_train.json?token=AAAAWLY264NANGVT3WFCFDLAVYG2A', lines=True)[['reviewText','sentiment']]
     if drop:
-        return data.dropna().reset_index(drop=True)
+       return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')
 
 def load_dev(drop=True) -> pd.DataFrame:
-    data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
+    data = pd.read_json('https://github.itu.dk/raw/frph/2ndyearproject/master/data/music_reviews_dev.json?token=AAAAWLYHC7D2KFTGTBH6TXDAVYG5O', lines=True)[['reviewText','sentiment']]
     if drop:
         return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')
 
 def load_test(drop=True) -> pd.DataFrame:
-    data = pd.read_json('../data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']]
+    data = pd.read_json('https://github.itu.dk/raw/frph/2ndyearproject/master/data/music_reviews_test_masked.json?token=AAAAWL7FGWYWSROIES2BJZTAVYG6O', lines=True)[['reviewText','sentiment']]
     if drop:
         return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')     
-
-def load_hard(drop=True) -> pd.DataFrame:
-    data = pd.read_json('../data/hard.json', lines=True)[['reviewText','sentiment']]
-    if drop:
-        return data.dropna().reset_index(drop=True)
-    else:
-        return data.fillna('')    
 
 
 def main():
     train = load_train()
     dev = load_dev()
     test = load_test()
-    hard = load_hard()
 
 if __name__ == '__main__':
     main()
