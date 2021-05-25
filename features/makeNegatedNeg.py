9140fbb18c758b96622d081b37cfc2e959a8fa8a
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-25 23:18:19 +0200

    Create makeNegatedNeg.py

---
+++ features/makeNegatedNeg.py
@@ -0,0 +1,56 @@
+import loader
+import pandas as pd
+import re
+from sys import argv
+
+def create(docs: list, search_tokens: list, regex_pattern='\w+\'\w|\w\w+',mode='count', export=False):
+    negations = [word.strip() for word in open('../data/negations.txt').readlines()]
+    negative_words = [word.strip() for word in open('../data/negative.txt').readlines()]
+    not_negative = set()
+    for word in negative_words:
+        for negation in negations:
+            not_negative.add(' '.join([negation,word]))
+
+    prog = re.compile(regex_pattern)
+
+    def count_negations(search_string,re_object,mode):
+        tokens = re_object.findall(search_string)
+        pairs = []
+        for i in range(len(tokens)-1):
+            pairs.append(' '.join([tokens[i],tokens[i+1]]))
+        negations_present = [word for word in pairs if word in not_negative]
+        if mode=='count':
+            return len(negations_present)
+        elif mode=='binary':
+            return 1 if negations_present else 0  
+
+    n_negations = [count_negations(sentence,prog,mode) for sentence in docs]
+
+
+    if export:
+        if mode=='count':
+            with open('../data/negated_negative_count_train.txt','w') as nf:
+                for line in n_negations:
+                    nf.write(str(line)+'\n')
+        elif mode=='binary':
+            with open('../data/negated_negative_bin_train.txt','w') as nf:
+                for line in n_negations:
+                    nf.write(str(line)+'\n')
+
+    return n_negations
+
+def main():
+    args = set(argv)
+    data = loader.load_train()['reviewText'].tolist()
+
+    search_tokens = [token.strip() for token in open('../data/non_neg.txt').readlines()]
+
+    #out = create(data,search_tokens,export=False,mode='binary')
+
+    if 'binary' in args:
+        out = create(data,search_tokens,export=True,mode='binary')
+    if 'count' in args:
+        out = create(data,search_tokens,export=True)
+
+if __name__ == '__main__':
+    main()
