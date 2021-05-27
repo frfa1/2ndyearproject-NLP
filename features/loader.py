51d7de58eccc407936292a40f6761141b96ae21f
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-27 16:24:00 +0200

    Update loader.py

--- features/loader.py
+++ features/loader.py
@@ -24,24 +24,74 @@ def load_train(drop=True,balance=True,shuffle=True) -> pd.DataFrame:
     else:
         return data.fillna('')
 
-def load_train_handcrafted():
+def load_train_handcrafted(balance=False):
     data = pd.read_json('../data/train_handcrafted.json')
+    if balance:
+        pos = data.loc[data["sentiment"] == "positive"]
+        neg = data.loc[data["sentiment"] == "negative"]
+        if len(pos) > len(neg):
+            bigger = pos
+            smaller = neg
+        elif len(neg) > len(pos):
+            bigger = neg
+            smaller = pos
+
+        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
+        data = pd.concat([bigger, smaller]).reset_index(drop=True)
     return data
 
 
-def load_dev(drop=True) -> pd.DataFrame:
+def load_dev(drop=True,balance=False) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
+
+    if balance:
+        pos = data.loc[data["sentiment"] == "positive"]
+        neg = data.loc[data["sentiment"] == "negative"]
+        if len(pos) > len(neg):
+            bigger = pos
+            smaller = neg
+        elif len(neg) > len(pos):
+            bigger = neg
+            smaller = pos
+
+        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
+        data = pd.concat([bigger, smaller]).reset_index(drop=True)
+
     if drop:
         return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')
 
-def load_dev_handcrafted():
+def load_dev_handcrafted(balance=False):
     data = pd.read_json('../data/dev_handcrafted.json')
+    if balance:
+        pos = data.loc[data["sentiment"] == "positive"]
+        neg = data.loc[data["sentiment"] == "negative"]
+        if len(pos) > len(neg):
+            bigger = pos
+            smaller = neg
+        elif len(neg) > len(pos):
+            bigger = neg
+            smaller = pos
+
+        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
+        data = pd.concat([bigger, smaller]).reset_index(drop=True)
     return data
 
-def load_test(drop=True) -> pd.DataFrame:
+def load_test(drop=True,balance=False) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']]
+    if balance:
+        pos = data.loc[data["sentiment"] == "positive"]
+        neg = data.loc[data["sentiment"] == "negative"]
+        if len(pos) > len(neg):
+            bigger = pos
+            smaller = neg
+        elif len(neg) > len(pos):
+            bigger = neg
+            smaller = pos
+
+        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
+        data = pd.concat([bigger, smaller]).reset_index(drop=True)
     if drop:
         return data.dropna().reset_index(drop=True)
     else:
