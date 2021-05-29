4f8463266d0b1d172b025ed7bcfdb20a132610d2
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-29 14:05:38 +0200

    Update makeIrrealisFeature.py

--- features/makeIrrealisFeature.py
+++ features/makeIrrealisFeature.py
@@ -6,16 +6,13 @@ from sys import argv
 def create(docs: list, mode='count', regex_pattern='\w+\'\w|\w\w+', export=False) -> list:
     irrealis = set(['should','could','would'])
     prog = re.compile(regex_pattern)
-
     def count_irrealis(search_string, re_object, mode):
         irrealis_present = [word for word in re_object.findall(search_string.lower()) if word in irrealis]
         if mode == 'count':
             return len(irrealis_present)
         elif mode == 'binary':
             return 1 if irrealis_present else 0  
-
     n_irrealis = [count_irrealis(sentence, prog, mode) for sentence in docs]
-
     if export:
         if mode=='count':
             with open('../data/irrealis_count_train.txt','w') as ir:
@@ -25,7 +22,6 @@ def create(docs: list, mode='count', regex_pattern='\w+\'\w|\w\w+', export=False
             with open('../data/irrealis_bin_train.txt','w') as ir:
                 for line in n_irrealis:
                     ir.write(str(line)+'\n')
-
     return n_irrealis
             
 
