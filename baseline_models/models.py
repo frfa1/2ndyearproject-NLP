0b18bb7d4d6c9400b3e77f1f8f4afb1e327544ed
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-29 16:42:57 +0200

    update loader with all handcrafted

--- baseline_models/models.py
+++ baseline_models/models.py
@@ -37,6 +37,39 @@ class LogisticRegressionBOW:
         return classification_report(labels, self.predict(text),digits=3)
 
 
+
+class LogisticRegressionBOWandHand:
+    def __init__(self) -> None:
+        self.bow_transformer = None
+        self.model = LogisticRegression(max_iter=1000)
+
+    def fit(self, features: pd.Series, labels: pd.Series) -> None:
+        self.bow_transformer = CountVectorizer(min_df=2).fit(features['reviewText'])
+        train_bow = self.bow_transformer.transform(features['reviewText'])
+        all_feat = pd.concat([train_bow,])
+        self.model.fit(train_bow,labels)
+
+    def predict(self, text: pd.Series) -> list:
+        test_bow = self.bow_transformer.transform(text)
+        predictions = self.model.predict(test_bow)
+        return predictions
+
+    def score(self, text: pd.Series,labels: pd.Series) -> float:
+        test_bow = self.bow_transformer.transform(text)
+        predictions = self.model.predict(test_bow)
+        return accuracy_score(labels,predictions)
+
+    def export_predict(self, text: pd.Series) -> None:
+        test_bow = self.bow_transformer.transform(text)
+        predictions = self.model.predict(test_bow)
+        out = [0 if item == 'negative' else 1 for item in predictions]
+        df = pd.DataFrame(out, columns=['prediction'])
+        df.to_csv('LogisticRegressionPredictions.csv',index_label='id')
+
+    def report(self, text: pd.Series, labels: pd.Series):
+        return classification_report(labels, self.predict(text),digits=3)
+
+
     
 
 def main():
@@ -49,8 +82,12 @@ def main():
         acc_score = clf.score(dev['reviewText'],dev['sentiment'])
         clf.export_predict(dev['reviewText'])
         print('Unit test finished successfully')
-    else:
-        pass
+    #else:
+       # pass
+
+    
+
+
 
 
 if __name__ == '__main__':
