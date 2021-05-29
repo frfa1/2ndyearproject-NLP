41a1bb8a883c1154825b7cc887936680a6011495
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-29 16:37:42 +0200

    Update loader.py

--- baseline_models/loader.py
+++ baseline_models/loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sys import argv
 
 def load_train(drop=True,balance=True,shuffle=True) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_train.json', lines=True)[['reviewText','sentiment']]
@@ -24,23 +24,6 @@ def load_train(drop=True,balance=True,shuffle=True) -> pd.DataFrame:
     else:
         return data.fillna('')
 
-def load_train_handcrafted(balance=False):
-    data = pd.read_json('../data/train_handcrafted.json')
-    if balance:
-        pos = data.loc[data["sentiment"] == "positive"]
-        neg = data.loc[data["sentiment"] == "negative"]
-        if len(pos) > len(neg):
-            bigger = pos
-            smaller = neg
-        elif len(neg) > len(pos):
-            bigger = neg
-            smaller = pos
-
-        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
-        data = pd.concat([bigger, smaller]).reset_index(drop=True)
-    return data
-
-
 def load_dev(drop=True,balance=False) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_dev.json', lines=True)[['reviewText','sentiment']]
 
@@ -61,22 +44,6 @@ def load_dev(drop=True,balance=False) -> pd.DataFrame:
         return data.dropna().reset_index(drop=True)
     else:
         return data.fillna('')
-
-def load_dev_handcrafted(balance=False):
-    data = pd.read_json('../data/dev_handcrafted.json')
-    if balance:
-        pos = data.loc[data["sentiment"] == "positive"]
-        neg = data.loc[data["sentiment"] == "negative"]
-        if len(pos) > len(neg):
-            bigger = pos
-            smaller = neg
-        elif len(neg) > len(pos):
-            bigger = neg
-            smaller = pos
-
-        bigger = bigger.iloc[:len(smaller)].reset_index(drop=True)
-        data = pd.concat([bigger, smaller]).reset_index(drop=True)
-    return data
 
 def load_test(drop=True,balance=False) -> pd.DataFrame:
     data = pd.read_json('../data/music_reviews_test_masked.json', lines=True)[['reviewText','sentiment']]
@@ -127,16 +94,44 @@ def load_movies(drop=True):
     else:
         return data.fillna('')
 
+def load_train_handcrafted():
+    data = pd.read_json('../data/train_handcrafted.json')
+    return data
+
+def load_dev_handcrafted():
+    data = pd.read_json('../data/dev_handcrafted.json')
+    return data
+
+def load_test_handcrafted():
+    data = pd.read_json('../data/test_handcrafted.json')
+    return data
+
+def load_movies_handcrafted():
+    data = pd.read_json('../data/movies_handcrafted.json')
+    return data
+
+
 
 def main():
-    train = load_train()
-    dev = load_dev()
-    test = load_test()
-    hard = load_hard()
-    movies = load_movies()
+    if 'ut' in argv:
+        train = load_train()
+        dev = load_dev()
+        test = load_test()
+        hard = load_hard()
+        movies = load_movies()
 
-    train_handcrafted = load_train_handcrafted()
-    dev_handcrafted = load_dev_handcrafted()
+        train_handcrafted = load_train_handcrafted()
+        dev_handcrafted = load_dev_handcrafted()
+        test_handcrafted = load_test_handcrafted()
+        movies_handcrafted = load_movies_handcrafted()
+
+        all_sets = [train,dev,test,hard,movies,train_handcrafted,dev_handcrafted,test_handcrafted,movies_handcrafted]
+
+        if 'print' in argv:
+            for dataset in all_sets:
+                print(dataset)
+
+        print('Loaded all datasets successfully!')
 
 
 if __name__ == '__main__':
