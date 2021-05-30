88e8954fe8384f2983ac0add33653acca3824124
Frederik Høngaard <mail@frederikhoengaard.com>
2021-05-31 01:12:16 +0200

    Update runNN.sh

--- hpc/runNN.sh
+++ hpc/runNN.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=runGRU
+#SBATCH --job-name=RNN_0
 #SBATCH --output=logs/job.%j.out
 #SBATCH --cpus-per-task=4
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu
 #SBATCH --partition=brown
 #SBATCH --mem=8G
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 echo Executing...
-python3 ../models/trainNN.py
+python3 ../models/RNN_0.py
 echo Finished!
